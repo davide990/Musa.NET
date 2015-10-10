dff4218e8fa74c94d82e4ddd6b0f88358ef5876c
Davide Guastella <davide.guastella90@gmail.com>
2015-10-10 17:31:20 +0200

    - Test projects changed to make them work under mono.

---
+++ FormulaLibraryTestProject/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace FormulaLibraryTestProject
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
